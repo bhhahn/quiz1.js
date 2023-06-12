1851289b4fd8163fc25ecf5d57d2ea9b3c4aaf07
bhhahhn <bhh@venezky.info>
2023-06-11 19:46:42 -0700

    First commit

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style ={styles.header}>
+        <Text color='#000F08'>Welcome to Ben's App Demo</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Add Players then Start the Game</Text>
+        <StatusBar style="auto" />
+        <Button
+          title="Start Game"
+          color="#4CB944"
+          onPress={() => Alert.alert('Game Started (Does not do anything right now)')}
+        />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#757780',
+    alignItems: 'start',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 3,
+    backgroundColor: '#FBF5F3',
     alignItems: 'center',
     justifyContent: 'center',
   },
